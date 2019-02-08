4049a78995a95a16b06db0f91154d97b3ed891c9
perlancar (on pc-office) <perlancar@gmail.com>
2019-02-08 15:40:40 +0700

    Initial commit

---
+++ lib/Module/CoreList/List.pm
@@ -0,0 +1,22 @@
+package Module::CoreList::List;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List of core modules by perl version
+
+=head1 DESCRIPTION
+
+This distribution contains POD pages that list core modules by perl version:
+
+# INSERT_MODULES_LIST /^Modules::CoreList::List::/
+
+The data comes from L<Module::CoreList> version:
+
+# COMMAND: perl -MModule::CoreList -E'say $Module::CoreList::VERSION'
+
+
+=head1 SEE ALSO
+
+L<Module::CoreList>
